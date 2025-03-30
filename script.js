8a5034aa045ec3d675d38b1b37dffd58954f5f80
Sivatheevan1224 <Sivatheevan1224@gmail.com>
2025-03-30 12:27:38 -0700

    Add review page Html,css,js code

--- script.js
+++ script.js
@@ -22,12 +22,36 @@ var swiper = new Swiper(".home-slider", {
     spaceBetween: 30,
     centeredSlides: true,
     autoplay: {
-      delay: 7500,
-      disableOnInteraction: false,
+    delay: 7500,
+    disableOnInteraction: false,
     },
     pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
+    el: ".swiper-pagination",
+    clickable: true,
     },
     loop:true ,
-  });
+});
+
+var swiper = new Swiper(".review-slider", {
+    spaceBetween: 20,
+    centeredSlides: true,
+    autoplay: {
+        delay: 7500,
+        disableOnInteraction: false,
+    },
+    loop: true,
+    breakpoints: {
+        0: {
+            slidesPerView: 1, // 1 slide per view on small screens
+        },
+        640: {
+            slidesPerView: 2, // 2 slides per view on medium screens
+        },
+        768: {
+            slidesPerView: 2, // 2 slides per view on tablets
+        },
+        1024: {
+            slidesPerView: 3, // 3 slides per view on larger screens
+        },
+    },
+});
